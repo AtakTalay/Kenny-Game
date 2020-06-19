cae2398a421a9cd5b5d8a2fce3946a3fc3a871c6
Ömer Ünlüsoy <omerunlusoy99@icloud.com>
2020-06-19 18:34:20 +0300

    Initial Commit

---
+++ kennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kennyGame
+//
+//  Created by Ömer Ünlüsoy on 19.06.2020.
+//  Copyright © 2020 Ömer Ünlüsoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
